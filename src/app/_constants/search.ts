1530de92655a32f150da250a6f0a3e4e2bef5126
jairo-de-lima <jairodelima94@gmail.com>
2024-08-10 16:07:11 -0300

    feat: add inicial babershop page

---
+++ src/app/_constants/search.ts
@@ -0,0 +1,31 @@
+interface QuickSearchOption {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOptions: QuickSearchOption[] = [
+  {
+    imageUrl: "/cabelo.svg",
+    title: "Cabelo",
+  },
+  {
+    imageUrl: "/barba.svg",
+    title: "Barba",
+  },
+  {
+    imageUrl: "/acabamento.svg",
+    title: "Acabamento",
+  },
+  {
+    imageUrl: "/hidratacao.svg",
+    title: "Hidratacao",
+  },
+  {
+    imageUrl: "/sombrancelhas.svg",
+    title: "Sombrancelhas",
+  },
+  {
+    imageUrl: "/massagem.svg",
+    title: "Massagem",
+  },
+]
