dea9c862c562858fd5ba1ef70ae8c9e4b8a52525
jairo-de-lima <jairodelima94@gmail.com>
2024-09-06 20:12:47 -0300

    feat: ensure bookings can be created

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -91,7 +91,11 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
         </h2>
         <div className="space-y-3">
           {barbershop.services.map((service: BarbershopService) => (
-            <ServiceItem key={service.id} service={service} />
+            <ServiceItem
+              key={service.id}
+              service={service}
+              barbershop={barbershop}
+            />
           ))}
         </div>
       </div>
