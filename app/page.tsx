1cbf5eeb0046eef17ae67a5f1dbed106a437099d
jairo-de-lima <jairodelima94@gmail.com>
2024-09-13 21:28:28 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const Home = async () => {
   const session = await getServerSession(authOptions)
@@ -46,8 +48,15 @@ const Home = async () => {
       <Header />
       {/* main content */}
       <div className="p-5">
-        <h2 className="text-xl font-bold">Ola Jairo</h2>
-        <p>Segunda-feira, 05 agosto</p>
+        <h2 className="text-xl font-bold">
+          Ola, {session?.user ? session.user.name : "Bem vindo"}{" "}
+        </h2>
+        <p>
+          <span className="capitalize">
+            {format(new Date(), "EEEE, dd", { locale: ptBR })}
+          </span>
+          <span> de {format(new Date(), "MMMM", { locale: ptBR })} </span>
+        </p>
         {/* busca */}
         <div className="mt-6">
           <Search />
